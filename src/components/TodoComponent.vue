30a73ee22faecd807a51ca9014ac88412dff38fc
michael dilulo <snoleopard01001101@gmail.com>
2020-10-28 12:40:00 -0700

    Todo Show up on page

--- src/components/TodoComponent.vue
+++ src/components/TodoComponent.vue
@@ -9,6 +9,7 @@
               type="text"
               name="newTodo"
               placeholder="Enter Todo Here..."
+              v-model="newTodo"
             />
             <button class="btn btn-success" type="submit">Submit Todo</button>
           </form>
@@ -17,20 +18,36 @@
     </div>
     <div class="container-fluid">
       <div class="row">
-        <div class="col-md-6"></div>
+        <div class="col-md-12" v-for="todo in todos" :key="todo.id">
+          {{ todo.content }}
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+// reactive variable that keeps track of what the user has input { ref } Reusable outside of Vue.js, variables objects respond to change
+import { ref } from "vue";
 export default {
   name: "todo-component",
   setup() {
+    // object wrapper, property called value on it
+    const newTodo = ref("");
+    const todos = ref([]);
+
     function createTodo() {
-      console.log("Todo was created");
+      todos.value.push({
+        id: Date.now(),
+        done: false,
+        content: newTodo.value,
+      });
+      newTodo.value = "";
     }
+    // makes it so you have access in the template
     return {
+      todos,
+      newTodo,
       createTodo,
     };
   },
